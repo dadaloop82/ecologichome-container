6e9ecc6e582154ba8caddde7897ae2376b8e9a5e
dadaloop82 <dadaloop82@gmail.com>
2023-11-27 16:42:58 +0000

    install and update svelte

--- ecologichome_src/frontend/.svelte-kit/generated/client-manifest.js
+++ ecologichome_src/frontend/.svelte-kit/generated/client-manifest.js
@@ -1,12 +1,12 @@
 export { matchers } from './client-matchers.js';
 
 export const components = [
-	() => import("..\\runtime\\components\\layout.svelte"),
-	() => import("..\\runtime\\components\\error.svelte"),
-	() => import("..\\..\\src\\routes\\About Us.svelte"),
-	() => import("..\\..\\src\\routes\\Controls.svelte"),
-	() => import("..\\..\\src\\routes\\Dashboard.svelte"),
-	() => import("..\\..\\src\\routes\\index.svelte")
+	() => import("../runtime/components/layout.svelte"),
+	() => import("../runtime/components/error.svelte"),
+	() => import("../../src/routes/About Us.svelte"),
+	() => import("../../src/routes/Controls.svelte"),
+	() => import("../../src/routes/Dashboard.svelte"),
+	() => import("../../src/routes/index.svelte")
 ];
 
 export const dictionary = {
